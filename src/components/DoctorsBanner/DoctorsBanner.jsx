f6db9905945c4d0d0b35c8926b489c5c080bb55d
erlan_akimov <erlan_akimov_dev@mail.ru>
2023-08-01 16:57:24 +0500

    banner for doctors

--- src/components/DoctorsBanner/DoctorsBanner.jsx
+++ src/components/DoctorsBanner/DoctorsBanner.jsx
@@ -1,11 +1,21 @@
 import styles from './DoctorsBanner.module.scss';
 import React from 'react';
+import doctors from '../../assets/images/doctors.png';
+import { Link } from 'react-router-dom';
 
-function DoctorsBanner () {
-    return (
-        <div className={styles.banner}>asdf
-        </div>
-    );
-};
+function DoctorsBanner() {
+	return (
+		<div className={styles.outer_wrapper}>
+			<div className={styles.banner}>
+				<h2 className={styles.title}>
+					У нас работают только квалифицированные специалисты
+				</h2>
+				<Link to='/doctors'><button className={styles.btn}>Записаться к врачу</button></Link>
+
+				<img className={styles.doctors_img} src={doctors} alt="" />
+			</div>
+		</div>
+	);
+}
 
 export default DoctorsBanner;
