dfed3846eaed06ec66eb0e7b26e211eff639ea66
erlan_akimov <erlan_akimov_dev@mail.ru>
2023-08-01 23:50:15 +0500

    update favicon

--- src/pages/Homepage/LicenseBar/LicenseBar.jsx
+++ src/pages/Homepage/LicenseBar/LicenseBar.jsx
@@ -1,17 +1,25 @@
 import styles from './LicenseBar.module.scss';
 import main_license from './main_license.png';
+import second_license from './2.jpg';
+import license1 from './img6-scaled.jpg';
+
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 function LicenseBar() {
+    const [status, setStatus] = useState('');
+    const handleImgChanger = () => {
+        
+    };
+
 	return (
 		<>
-        <h1 className={styles.main_title}>Документы</h1>
+			<h1 className={styles.title}>Документы</h1>
+            <Link to='/documents'><div className={styles.link}>cмотреть все</div></Link>
 			<div className={styles.wrapper}>
-				<div className={styles.license_item}>
-					<img className={styles.license_img} src={main_license} alt="" />
-					<h4 className={styles.title}>
-						Лицензия на осуществление медицинской деятельности
-					</h4>
-				</div>
+				<img className={styles.license_img} src={second_license} alt="" />
+				<img className={styles.license_img} src={main_license} alt="" />
+				<img className={styles.license_img} src={license1} alt="" />
 			</div>
 		</>
 	);
