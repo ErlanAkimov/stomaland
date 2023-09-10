7a0778acc366081842f2f04e68786b1c99d61d97
erlan_akimov <erlan_akimov_dev@mail.ru>
2023-09-10 14:18:48 +0500

    snap

--- src/pages/Homepage/Steps/Steps.jsx
+++ src/pages/Homepage/Steps/Steps.jsx
@@ -3,7 +3,11 @@ import styles from './steps.module.scss'
 function Steps () {
     return(
         <div className={styles.wrapper}>
-            
+
+            <div className={styles.title}>5 правил идеальной улыбки</div>
+            <div className={styles.wrapper_inner}>
+                
+            </div>
         </div>
     )
 }
